63f2dc6ca7534a6dbec45185422ea386527f5ec8
Ryan <etrigan976@gmail.com>
2026-07-12 14:34:38 -0700

    Adjustment to Items Stack

--- scripts/item_stack.gd
+++ scripts/item_stack.gd
@@ -1,140 +1,37 @@
-extends CharacterBody2D
+extends Node2D
 
-@export_group("Movement Parameters")
-@export var speed : float = 120 
-@export var accel : float = 80 
-@export_subgroup("Jump Parameters")
-@export var jump_height : float = 128 
-@export var time_to_peak_seconds : float = 0.5 
-@export var time_to_ground_seconds : float = 0.25 
-@export var variable_jump_height := true
-@export var max_coyote_time : float = 0.05
-@export var max_jump_buffer : float = 0.05
+@export var item_scene: PackedScene = preload("res://scenes/items/test_item.tscn")
+@export var vertical_spacing: float = 24.0
 
-@export_group("Item Stack Options")
-@export var item_stack_scene: PackedScene 
-@export var base_knock_off_threshold: float = 650.0
+var stack: Array[Dictionary] = []
 
-var jump_force : float
-var gravity : float 
-var jump_gravity : float 
+func push_item(item_data: Dictionary) -> void:
+	stack.append(item_data)
+	_update_visual_stack()
 
-var coyote_timer := 0.0
-var jump_buffer_timer := 0.0
-var was_on_floor := false 
-var is_jumping := false
-
-var last_velocity := Vector2.ZERO
-var facing_direction := Vector2.RIGHT
-var item_stack: Node2D = null 
-
-func _ready() -> void:
-	jump_force = 2.0 * jump_height / (time_to_peak_seconds)
-	jump_gravity = jump_force / (time_to_peak_seconds * 60)
-	gravity = 2.0 * jump_height / (time_to_ground_seconds * time_to_ground_seconds * 60)
-	
-	accel *= 60
-	gravity *= 60
-	jump_gravity *= 60
-
-	if item_stack_scene:
-		item_stack = item_stack_scene.instantiate() as Node2D
-		add_child(item_stack)
-		item_stack.position = Vector2(0, -16) 
-	else:
-		push_warning("ItemStackScene is missing from the Player inspector configuration!")
-
-
-func _physics_process(delta: float) -> void:	
-	horizontal_movement(delta)
-	jump(delta)
-	
-	check_stack_stability(delta)
-	
-	apply_velocity(delta)
-
-
-func horizontal_movement(delta: float):
-	var vel := 0.0
-	if Input.is_action_pressed("left"):
-		vel -= speed
-		facing_direction = Vector2.LEFT
-	if Input.is_action_pressed("right"):
-		vel += speed
-		facing_direction = Vector2.RIGHT
-	
-	velocity.x = move_toward(velocity.x, vel, accel * delta)
-	
-	
-func jump(delta: float):
-	var on_floor = is_on_floor()
-	var should_jump = false
-	if Input.is_action_just_pressed("jump"):
-		if on_floor or coyote_timer > 0:
-			should_jump = true
-		else:
-			jump_buffer_timer = max_jump_buffer
-	elif jump_buffer_timer > 0 and on_floor:
-		should_jump = true
-	
-	if should_jump:
-		is_jumping = true
-		velocity.y = -jump_force
-	
-	if Input.is_action_just_released("jump") and variable_jump_height:
-		is_jumping = false
-	
-	if on_floor:
-		return
-	
-	if is_jumping:
-		velocity.y += jump_gravity * delta
-	else:
-		velocity.y += gravity * delta
-		
-
-func apply_velocity(delta : float):
-	coyote_timer = max(coyote_timer - delta, 0)
-	jump_buffer_timer = max(jump_buffer_timer - delta, 0)
-	
-	if not is_on_floor() and was_on_floor and velocity.y >= 0:
-			coyote_timer = max_coyote_time
-	
-	was_on_floor = is_on_floor()
-	
-	var pre_slide_velocity = velocity
-	move_and_slide()
-	
-	if is_on_wall() and pre_slide_velocity.length() > 100.0:
-		knock_off_item()
-
-
-func check_stack_stability(delta: float) -> void:
-	if not item_stack or item_stack.get_stack_height() == 0:
+func pop_item(spawn_position: Vector2, throw_velocity: Vector2) -> void:
+	if stack.is_empty():
 		return
 		
-	var current_acceleration = (velocity - last_velocity) / delta
-	last_velocity = velocity
+	var item_data = stack.pop_front() # FIFO stack popping
 	
-	var height_penalty = 1.0 + (item_stack.get_stack_height() * 0.25)
-	var modified_threshold = base_knock_off_threshold / height_penalty
+	var new_item = item_scene.instantiate()
+	get_tree().current_scene.add_child(new_item)
+	new_item.global_position = spawn_position
 	
-	if current_acceleration.length() > modified_threshold:
-		knock_off_item()
+	# Apply properties...
+	new_item.on_dropped(throw_velocity)
+	_update_visual_stack()
 
+func _update_visual_stack() -> void:
+	for child in get_children():
+		child.queue_free()
+	for i in range(stack.size()):
+		var placeholder = Sprite2D.new()
+		placeholder.texture = preload("res://icon.svg")
+		placeholder.scale = Vector2(0.5, 0.5)
+		placeholder.position = Vector2(0, -i * vertical_spacing)
+		add_child(placeholder)
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact"):
-		drop_item_voluntarily()
-
-
-func drop_item_voluntarily() -> void:
-	if item_stack and item_stack.get_stack_height() > 0:
-		var toss_vector = (facing_direction * 140.0) + Vector2(0, -80.0)
-		item_stack.pop_item(global_position + (facing_direction * 16), toss_vector)
-
-
-func knock_off_item() -> void:
-	if item_stack and item_stack.get_stack_height() > 0:
-		var chaos_vector = Vector2(randf_range(-100, 100), -200.0)
-		item_stack.pop_item(global_position + Vector2(0, -24), chaos_vector)
+func get_stack_height() -> int:
+	return stack.size()
