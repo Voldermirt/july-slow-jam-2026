c236eea49132142ae4d7bf466978a213d2e1033a
Spencer Henwood <henwood@pdx.edu>
2026-07-12 01:39:45 -0700

    implemented base interaction handling

--- scripts/items/base_item.gd
+++ scripts/items/base_item.gd
@@ -1,17 +1,29 @@
 class_name BaseItem extends RigidBody2D
-## This is the abstract base class of the Item hierarchy. 
+## This is the base class of the Item hierarchy that defines the default 
+## behavior of an item.
 ##
 ## Contains the attributes common to all items (weight and value), tracks the 
-## item's state, and provides abstract methods for derived classes to implement 
-## which define the item's behavior when dropped, placed, and interacted with.
+## item's state, and provides methods (which derived classes can override) that
+## define the item's behavior when dropped, placed, and interacted with.
 
 @export_category("Item Attributes") # exported for testing
-@export_range(0.0, 2.0, 0.1) 	var weight: float = 1.0
+## Weight: The weight of the item while on the stack. 
+## TODO how does this interact with mass? is this Weven necessary?
+#@export_range(0.0, 2.0, 0.1) 	var weight: float = 1.0
+## Value: The number of points this item is worth when delivered
 @export_range(0, 10, 1) 		var value: int = 5
+
+@export_category("Physics Properties")
+## How much the item accelerates when placed.
+@export var base_speed: float = 100.0
+## The y value for the angle an item gets pushed when placed
+@export var place_angle: float = -0.5
 
 @export_category("Child Nodes")
 @export var sprite: Sprite2D
 @export var collision: CollisionShape2D
+@export var interact_area: Area2D
+
 
 # States
 # on_stack: whether the item is currently on the stack.
@@ -20,29 +32,65 @@ class_name BaseItem extends RigidBody2D
 var on_stack: bool
 var is_placed: bool
 
-## Override in derived class to set item attributes when object is created via
-## .new()
-func _init() -> void:
-	pass
+func _ready() -> void:
+	# Report collisions when frozen
+	contact_monitor = true
+	max_contacts_reported = 5
+	# Don't use custom integrator yet
+	custom_integrator = false
+	freeze_mode = RigidBody2D.FREEZE_MODE_STATIC
+	# Connect signals
+	interact_area.body_entered.connect(handle_body_entered)
+	sleeping_state_changed.connect(handle_sleeping_state_changed)
+	# Set collision layer
+	set_collision_layer_value(2, true)
+	set_collision_mask_value(1, true)
+	set_collision_mask_value(2, true)
+	interact_area.set_collision_layer_value(2, true)
+	interact_area.set_collision_mask_value(1, true)
+	interact_area.set_collision_mask_value(2, true)
 
-func _physics_process(delta: float) -> void:
-	pass
-	#TODO handle gravity if on_stacked and is_placed are both false
+func _init() -> void:
+	on_stack = true
+	is_placed = false
 
 ## Called when the item is instantiated while being knocked off the stack.
 ## Args:
 ## 		velocity: The input velocity of the item being knocked off. 
 func knock_down(velocity: Vector2) -> void:
-	pass
+	if not on_stack: # item has already been dropped
+		return
+	on_stack = false
+	
+	apply_impulse(velocity)
 
-
-## Called when the item is instantiated while being placed. This is 
+## Called when the item is instantiated while being intentionally placed.
 ## Args:
-## 		direction: The direction the player is facing. 0 = left, 1 = right.
-##		player_velocity: The player's velocity at the moment the item is placed.
-func place(direction: int) -> void:
+## 		direction: The direction the player is facing. -1 = left, 1 = right.
+func drop(direction: int) -> void:
+	if not on_stack:
+		return
+	on_stack = false
+	
+	apply_impulse(Vector2(direction, place_angle) * base_speed)
+
+## Called when the item lands and stops moving. Locks item in place.
+func on_land() -> void:
+	if is_placed:
+		return
+	is_placed = true
+	set_deferred("freeze", true)
+	
+## Called when player or other item touches this item.
+func on_interact(body) -> void:
 	pass
 
-## Called when the item lands and stops moving.
-func on_land() -> void:
-	pass
+## Called when something collides with the item.
+func handle_body_entered(body: Node) -> void:
+	if body is CharacterBody2D or body is BaseItem:
+		on_interact(body)
+
+## Triggered when the item stops moving for a certain period off time.
+func handle_sleeping_state_changed():
+	if sleeping:
+		on_land()
--- scripts/items/test_item.gd
+++ scripts/items/test_item.gd
@@ -1,19 +1,27 @@
-extends BaseItem
+class_name TestItem extends BaseItem
 
-@export_category("Properties")
-@export var place_speed: float = 5.0
+func _init() -> void:
+	super._init()
+	freeze = true
 
 func knock_down(velocity: Vector2) -> void:
-	if not on_stack: # item has already been dropped
-		return
-	
-	on_stack = false
-	linear_velocity = velocity
-	angular_velocity = 1
+	super.knock_down(velocity)
 
-func place(direction: int) -> void:
-	if not on_stack:
+func drop(direction: int) -> void:
+	super.drop(direction)
+
+func on_land() -> void:
+	super.on_land()
+	sprite.flip_v = true
+
+func on_interact(body) -> void:
+	sprite.flip_v = not sprite.flip_v
+
+## Test function that knocks the object down when clicked
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if not event.is_action_pressed("interact"):
 		return
 	
-	on_stack = false
-	linear_velocity = Vector2(1, -0.5) * place_speed
+	freeze = false
+	knock_down(Vector2(-1, 0.5) * 200)
+	#place(1)
