6847d3a0133253ce1e42b81940f7c803062cea34
Voldermirt <emars.nighting@gmail.com>
2026-07-20 18:28:26 -0700

    Finish scene changes

--- scripts/order_destination.gd
+++ scripts/order_destination.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 
 func _on_interactable_component_on_interact(player: Player) -> void:
-	get_tree().get_first_node_in_group("order_manager").order_destination_interacted(self, player)
+	get_tree().get_first_node_in_group("order_manager").order_destination_interacted(global_position, player)
